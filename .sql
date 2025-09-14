1db55757c1a83e1f7152e5aed82832be73ed1cba
Irene-ops-ai <ireneouko45@gmail.com>
2025-09-14 12:07:01 +0300

    Create .sql

---
+++ .sql
@@ -0,0 +1,8 @@
+https://github.com/PLP-Database-DEPT/hands-on-example/tree/main/week-1
+https://github.com/PLP-Database-DEPT/hands-on-example/tree/main/week-2
+https://github.com/PLP-Database-DEPT/hands-on-example/tree/main/week-3
+https://github.com/PLP-Database-DEPT/hands-on-example/tree/main/week-4
+https://github.com/PLP-Database-DEPT/hands-on-example/tree/main/week-5
+https://github.com/PLP-Database-DEPT/hands-on-example/tree/main/week-6
+https://github.com/PLP-Database-DEPT/hands-on-example/tree/main/week-7
+https://github.com/PLP-Database-DEPT/hands-on-example/tree/main/week-7
